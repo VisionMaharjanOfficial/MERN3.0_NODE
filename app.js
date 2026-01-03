e3de3c4b9e8dae0bc8b5fd98d0ca224ecd87da92
VisionMaharjanOfficial <visionmhrzn1@gmail.com>
2026-01-03 19:51:31 +0545

    added contact page

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ app.get("/about",(req,res)=>{
         message:"this is aboutpage"
     })
 })
+app.get("/contact",(req,res)=>{
+    res.json({
+        message:"this is contactpage"
+    })
+})
 app.listen(3000,()=>{
     console.log("node js project has started")
 })
